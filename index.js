4b69160b1417b7e002a18aac6c822cdc427f28f9
Laura Beutler <labeutler@gmail.com>
2023-01-24 07:44:11 -0500

    adding list of requirments needed for index.js

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 const mysql = require("mysql2");
 const db = require("./db");
+
+// Required information needed:
+
+    // WHAT WOULD YOU LIKE TO DO? (Use arrow keys to select an option)
+        //View All Employees
+            //pull list of all employees showing ID, FIRST & LAST NAME, TITLE, DEPARTMENT, SALARY & MANAGER
+            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
+        //Add Employee
+            //WHAT IS THE EMPLOYEE'S FIRST NAME?
+            //WHAT IS THE EMPLOYEE'S LAST NAME?
+            //WHAT IS THE EMPLOYEE'S ROLE? (USE ARROW KEYS) PROVIDE LIST OF ROLES.
+            //WHO IS THE EMPLOYEE'S MANAGER? (USE ARROW KEYS)PROVIDE MANAGER LIST AND "NONE" OPTION
+            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
+        //Update Employee Role
+            //WHICH EMPLOYEE'S ROLE DO YOU WANT TO UPDATE? (USE ARROW KEYS) PROVIDE LIST OF EMPLOYEES
+            //WHICH ROLE DO YOU WANT TO ASSIGN THE SELECTED EMPLOYEE? (USE ARROW KEYS) PROVIDE LIST OF ROLES
+            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
+        //View ALL Roles
+            //pull list of roles showing ID, TITLE, DEPARTMENT & SALARY
+            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
+        //Add Role
+            //WHAT IS THE NAME OF THE ROLE?
+            //WAHT IS THE SALARY OF THE ROLE?
+            //WHICH DEPARTMENT DOES THE ROLE BELONG TO? (USE ARROW KEYS) 
+            //PROVIDE LIST OF DEPARTMENTS
+            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
+        //View All Departments
+            //pull list of all departments showing ID & NAME
+            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
+        //Add Department
+            //WHAT IS THE NAME OF THE DEPARTMENT?
+            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
+        //Quit
+
+//WHAT IS THE NAME OF THE DEPARTMENT?
+
+//WHAT WOULD YOU LIKE TO DO?
+
+//WHAT IS THE NAME OF THE ROLE?
+
+//WHAT IS THE SALARY OF THE ROLE?
+
+//WHICH DEPARMENT DOES THE ROLE BELONG TO?
+//ENGINEERING
+//FINANCE
+//LEGAL
+//SALES
+
+
