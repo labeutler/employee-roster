1cace1e4d70d7a231d481ac31dc2179b7388afdf
Laura Beutler <labeutler@gmail.com>
2023-01-24 08:17:55 -0500

    adding requirements for prompts, with start of functions to index.js

--- index.js
+++ index.js
@@ -1,51 +1,122 @@
 const mysql = require("mysql2");
+const { allowedNodeEnvironmentFlags } = require("process");
 const db = require("./db");
 
 // Required information needed:
 
-    // WHAT WOULD YOU LIKE TO DO? (Use arrow keys to select an option)
-        //View All Employees
-            //pull list of all employees showing ID, FIRST & LAST NAME, TITLE, DEPARTMENT, SALARY & MANAGER
-            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
-        //Add Employee
-            //WHAT IS THE EMPLOYEE'S FIRST NAME?
-            //WHAT IS THE EMPLOYEE'S LAST NAME?
-            //WHAT IS THE EMPLOYEE'S ROLE? (USE ARROW KEYS) PROVIDE LIST OF ROLES.
-            //WHO IS THE EMPLOYEE'S MANAGER? (USE ARROW KEYS)PROVIDE MANAGER LIST AND "NONE" OPTION
-            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
-        //Update Employee Role
-            //WHICH EMPLOYEE'S ROLE DO YOU WANT TO UPDATE? (USE ARROW KEYS) PROVIDE LIST OF EMPLOYEES
-            //WHICH ROLE DO YOU WANT TO ASSIGN THE SELECTED EMPLOYEE? (USE ARROW KEYS) PROVIDE LIST OF ROLES
-            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
-        //View ALL Roles
-            //pull list of roles showing ID, TITLE, DEPARTMENT & SALARY
-            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
-        //Add Role
-            //WHAT IS THE NAME OF THE ROLE?
-            //WAHT IS THE SALARY OF THE ROLE?
-            //WHICH DEPARTMENT DOES THE ROLE BELONG TO? (USE ARROW KEYS) 
-            //PROVIDE LIST OF DEPARTMENTS
-            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
-        //View All Departments
-            //pull list of all departments showing ID & NAME
-            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
-        //Add Department
-            //WHAT IS THE NAME OF THE DEPARTMENT?
-            //PROMPT TO RETURN TO WHAT WOULD YOU LIKE TO DO?
-        //Quit
-
-//WHAT IS THE NAME OF THE DEPARTMENT?
-
-//WHAT WOULD YOU LIKE TO DO?
-
-//WHAT IS THE NAME OF THE ROLE?
-
-//WHAT IS THE SALARY OF THE ROLE?
-
-//WHICH DEPARMENT DOES THE ROLE BELONG TO?
-//ENGINEERING
-//FINANCE
-//LEGAL
-//SALES
 
 
+// Create function to prompt list for what you would like to select from.
+function initialPrompt() {
+    prompt([
+        {
+            type: "list",
+            name: "selection",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View ALL Employees",
+                    enter: "View_Employees"
+                },
+                {
+                    name: "Add Employee",
+                    enter: "Add_Employee"
+                },
+                {
+                    name: "Update Employee Role",
+                    enter: "Update_Employee_Role"
+                },
+                {
+                    name: "View ALL Roles",
+                    enter: "View_Roles"
+                },
+                {
+                    name: "Add Role",
+                    enter: "Add_Roll"
+                },
+                {
+                    name: "View ALL Departments",
+                    enter: "View_All_Departments"
+                },
+                {
+                    name: "Add Department",
+                    enter: "Add_Department"
+                },
+                {
+                    name: "Quit",
+                    enter: "Quit"
+                }
+            ]
+        }
+    ]).then(res => {
+        //Call needed prompts for selection made above.
+        let choice = res.choice;
+        switch (choice) {
+            case "View_Employees":
+                viewEmployees();
+                break;
+            case "Add_Employee":
+                addEmployee();
+                break;
+            case "Update_Employee_Role":
+                updateEmployeeRole();
+                break;
+            case "View_Roles":
+                viewRoles();
+                break;
+            case "Add_Roll":
+                addRole();
+                break;
+            case "View_All_Departments":
+                viewDepartments();
+                break;
+            case "Add_Department":
+                addDepartment();
+                break;
+            case "Quit":
+                quit;
+                break;
+        }
+    })
+}
+
+// In order to see the requested information, provide where it's coming from
+// To View All Employees
+function viewEmployees() {
+    
+}
+
+// To Add Employee
+function addEmployee() {
+
+}
+
+// To Update Employee Role
+function updateEmployeeRole() {
+
+}
+
+// To View All Roles
+function viewRoles() {
+
+}
+
+// To Add Role
+function addRole() {
+
+}
+
+// To View All Departments
+function viewDepartments() {
+
+}
+
+// To Add Department
+function addDepartment() {
+
+}
+
+// To Quit
+function quit() {
+
+}
